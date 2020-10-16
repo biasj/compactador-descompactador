9237eb92d355177b27279aa62c894c541f49f2b7
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2020-10-16 13:31:46 -0300

    Junta todas as partes

--- src/compactador_v1/Compactador.java
+++ src/compactador_v1/Compactador.java
@@ -43,7 +43,7 @@ public class Compactador {
             while (linha != null && !linha.equals("0")) {
                 
                 Pattern lpPalavra = Pattern.compile("[a-zA-Z]+");
-                Pattern lpNaoPalavra = Pattern.compile("[^ a-zA-Z]");
+                Pattern lpNaoPalavra = Pattern.compile("[^a-zA-Z]");
 
                 Matcher lmPalavra = lpPalavra.matcher(linha);
                 Matcher lmNaoPalavra = lpNaoPalavra.matcher(linha);
@@ -56,18 +56,44 @@ public class Compactador {
 
                 while (lbExistePalavra) {
                     
-                    System.out.println("Posição da Palavra: " + lmPalavra.start() + " Termina em: " + lmPalavra.end());
+//                    System.out.println("Posição da Palavra: " + lmPalavra.start() + " Termina em: " + lmPalavra.end());
                     
                     // Se a palavra está no início da linha
                     if (lmPalavra.start() != PosicaoLinha) {
-                        naoPalavra = linha.substring(PosicaoLinha, lmPalavra.start()-1);
-                        System.out.println("Não Palavra:" + naoPalavra + "|");
+                        naoPalavra = linha.substring(PosicaoLinha, lmPalavra.start());
+//                        System.out.println("Não Palavra:" + naoPalavra + "|");
+                        writer.write(naoPalavra);
                     }
 
                     // retirar a palavra da linha desde a posição start até end -1 e armazenar em uma variável
                     palavra = linha.substring(lmPalavra.start(), lmPalavra.end());
 
-                    System.out.println("Palavra:" + palavra + "|");
+//                    System.out.println("Palavra:" + palavra + "|");
+                    
+                    // LOGICA 
+                    No temp = lista.buscaLinear(palavra);
+
+                    // se palavra não estiver na lista
+                    if (temp == null) {
+                        // insere no início da lista
+                        lista.insereInicio(palavra);
+
+                        // escreve no arquivo compactado 
+                        writer.write(palavra);
+
+                    } else {
+                        // pega a posição na lista e escreve no arquivo 
+                        // como pegar a posição do No na lista?
+                        int posicao = lista.buscaPosicao(palavra);
+                        writer.write(String.valueOf(posicao));
+
+                        // remove palavra da lista
+                        lista.removePalavra(palavra);
+
+                        // insere no início da lista
+                        lista.insereInicio(palavra);
+
+                    }
                     
                     // atualizar a nova posição de leitura na linha
                     PosicaoLinha = lmPalavra.end();
@@ -79,43 +105,11 @@ public class Compactador {
                 
                 if (PosicaoLinha < linha.length()) {
                     naoPalavra = linha.substring(PosicaoLinha, linha.length());
-                    System.out.println("Não Palavra:" + naoPalavra + "|");
+//                    System.out.println("Não Palavra:" + naoPalavra + "|");
+                    writer.write(naoPalavra);
                 }
-                
-                // se ainda existe algo na linha que não seja uma palavra
-                
-                
-                
-                
-                linhaEntrada = linha.split(" ");
-
-                // para cada palavra na linha, conferir se está na lista e copiar a palavra ou a posição na lista para o arquivo
-                for (int i = 0; i < linhaEntrada.length; i++) {
-                    No temp = lista.buscaLinear(linhaEntrada[i]);
-
-                    // se palavra não estiver na lista
-                    if (temp == null) {
-                        // insere no início da lista
-                        lista.insereInicio(linhaEntrada[i]);
-
-                        // escreve no arquivo compactado 
-                        writer.write(linhaEntrada[i]);
-
-                    } else {
-                        // pega a posição na lista e escreve no arquivo 
-                        // como pegar a posição do No na lista?
-                        int posicao = lista.buscaPosicao(linhaEntrada[i]);
-                        writer.write(String.valueOf(posicao));
-
-                        // remove palavra da lista
-                        lista.removePalavra(linhaEntrada[i]);
-
-                        // insere no início da lista
-                        lista.insereInicio(linhaEntrada[i]);
-
-                    }
-                }
-                
+                    
+                writer.write("\n");
                 // lê a próxima linha
                 linha = reader.readLine();
                 PosicaoLinha = 0;
@@ -150,7 +144,7 @@ public class Compactador {
                 
                 while (linha != null && !linha.equals("0")) {
 
-                    linhaEntrada = linha.split(" ");
+                    linhaEntrada = linha.split("\\b");
 
                     // para cada palavra na linha, conferir se está na lista e copiar a palavra ou a posição na lista para o arquivo
                     for (int i = 0; i < linhaEntrada.length; i++) {
@@ -159,13 +153,19 @@ public class Compactador {
                         if(linhaEntrada[i].matches("[A-Za-z]+")) {
                             No temp = lista.buscaLinear(linhaEntrada[i]);
 
+                            // escreve no arquivo compactado 
+                            writer.write(linhaEntrada[i]);
+                            
                             // se palavra não estiver na lista
                             if (temp == null) {
                                 // insere no início da lista
                                 lista.insereInicio(linhaEntrada[i]);
 
-                                // escreve no arquivo compactado 
-                                writer.write(linhaEntrada[i] + " ");
+                                
+                            } else {
+       
+                                lista.removePalavra(linhaEntrada[i]);
+                                lista.insereInicio(linhaEntrada[i]);
                             }
                             
                         } else if(linhaEntrada[i].matches("[0-9]+")){
@@ -173,19 +173,20 @@ public class Compactador {
                             // como pegar a posição do No na lista?
                             int posicao = Integer.parseInt(linhaEntrada[i]);
                             No temp = lista.buscaPalavra(posicao);
-                            writer.write(temp.getElemento() + " ");
+                            writer.write(temp.getElemento());
 
                             // remove palavra da lista
                             lista.removePalavra(temp.getElemento());
                             
                             // insere no início da lista
                             lista.insereInicio(temp.getElemento());
-                            
-                            
 
+                        } else {
+                            writer.write(linhaEntrada[i]);
                         }
                     }
-
+                    
+                    writer.write("\n");
                     // lê a próxima linha
                     linha = reader.readLine();
                 }
